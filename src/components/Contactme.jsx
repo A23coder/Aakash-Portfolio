1992f047e277012861fa6bd94b133c381ac6c13e
A23coder <91893285+A23coder@users.noreply.github.com>
2024-10-08 10:48:08 +0530

    working contact me page

--- src/components/Contactme.jsx
+++ src/components/Contactme.jsx
@@ -3,7 +3,7 @@ import {  motion } from "framer-motion";
 import { useForm, ValidationError } from '@formspree/react';
 
 function Contactme() {
-  const [state, handleSubmit] = useForm("mnqeykrz");
+  const [state, handleSubmit] = useForm("manywlvv");
   if (state.succeeded) {
       return <p className='text-center m-5 '>Thanks for your reply!</p>;
   }
@@ -30,56 +30,75 @@ function Contactme() {
                 <div>
                     <div className="max-w-full mx-auto rounded-lg overflow-hidden">
                     <motion.form 
-                whileInView={{opacity:1, x:0}}
-               initial={{opacity:0,x:200}} transition={{duration: 0.8, delay: 1.5}}
-               onSubmit={handleSubmit} className="space-y-8">
-                  <div>
-                    <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-                      Your email
-                    </label>
-                    <input
-                      type="email"
-                      id="email"
-                      name="email"
-                      className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-200 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-                      placeholder="name@flowbite.com"
-                      required
-                    />
-                  </div>
-                  <div>
-                    <label htmlFor="subject" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-                      Your phone no
-                    </label>
-                    <input
-                      type="text"
-                      id="subject"
-                      name="phone no"
-                      className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-200 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-                      placeholder="+91 xxxx xxxx xx"
-                      required
-                    />
-                  </div>
-                  <div className="sm:col-span-2">
-                    <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-                      Your message
-                    </label>
-                    <textarea
-                      id="message"
-                      name="message"
-                      rows="6"
-                      className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-200 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                      placeholder="Leave a comment..."
-                      required
-                    ></textarea>
-                  </div>
-                  <button
-                  disabled={state.submitting}
-                    type="submit"
-                    className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-sky-900 sm:w-fit  focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                  >
-                    Send message
-                  </button>
-                </motion.form>
+  onSubmit={handleSubmit}
+  className="space-y-8"
+>
+  <div>
+    <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+      Your email
+    </label>
+    <input
+      type="email"
+      id="email"
+      name="email"
+      className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-200 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+      placeholder="name@flowbite.com"
+      required
+    />
+    <ValidationError 
+      prefix="Email" 
+      field="email"
+      errors={state.errors}
+    />
+  </div>
+
+  <div>
+    <label htmlFor="subject" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+      Your phone no
+    </label>
+    <input
+      type="text"
+      id="subject"
+      name="phone no"
+      className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-200 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+      placeholder="+91 xxxx xxxx xx"
+      required
+    />
+    <ValidationError 
+      prefix="Phone" 
+      field="phone no"
+      errors={state.errors}
+    />
+  </div>
+
+  <div className="sm:col-span-2">
+    <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+      Your message
+    </label>
+    <textarea
+      id="message"
+      name="message"
+      rows="6"
+      className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-200 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+      placeholder="Leave a comment..."
+      required
+    ></textarea>
+    <ValidationError 
+      prefix="Message" 
+      field="message"
+      errors={state.errors}
+    />
+  </div>
+
+  <button
+    disabled={state.submitting}
+    type="submit"
+    className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-sky-900 sm:w-fit focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+  >
+    Send message
+  </button>
+</motion.form>
+
                     </div>
                 </div>
             </div>
